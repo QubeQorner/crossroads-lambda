8376cee779e707bfd25af8c148a7c0b56b6992ff
Gareth Walkersmith <gpsmith83@gmail.com>
2020-11-01 15:03:26 +0000

    Added pipeline and buildspec.yaml

---
+++ pipeline/bin/pipeline.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { PipelineStack } from '../lib/pipeline-stack';
+
+if (!process.env.GITHUB_TOKEN) {
+    console.log("No Github Token present");
+}
+
+const app = new cdk.App();
+new PipelineStack(app, 'crossroads-ci-cd');
---
+++ pipeline/lib/pipeline-stack.ts
@@ -0,0 +1,61 @@
+import * as cdk from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
+import codepipeline = require('@aws-cdk/aws-codepipeline');
+import codepipeline_actions = require('@aws-cdk/aws-codepipeline-actions');
+import codebuild = require('@aws-cdk/aws-codebuild');
+import {SecretValue} from "@aws-cdk/core";
+
+export class PipelineStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const artifactsBucket = new s3.Bucket(this, "CrossroadsArtifactsBucket");
+
+    const pipeline = new codepipeline.Pipeline(this, 'CrossroadsPipeline', {
+      artifactBucket: artifactsBucket
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+          new codepipeline_actions.GitHubSourceAction({
+            actionName: 'Checkout',
+            output: sourceOutput,
+            owner: 'QubeQorner',
+            repo: 'crossroads-lambda',
+            branch: 'main',
+            oauthToken: SecretValue.secretsManager('arn:aws:secretsmanager:eu-central-1:597880501761:secret:github-token-8Ehk8E'),
+            trigger: codepipeline_actions.GitHubTrigger.WEBHOOK
+          })
+      ]
+    });
+
+    const buildOutput = new codepipeline.Artifact();
+
+    // Declare a new CodeBuild project
+    const buildProject = new codebuild.PipelineProject(this, 'Build', {
+      environment: { buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_2 },
+      environmentVariables: {
+        'PACKAGE_BUCKET': {
+          value: artifactsBucket.bucketName
+        }
+      }
+    });
+
+    // Add the build stage to our pipeline
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Build',
+          project: buildProject,
+          input: sourceOutput,
+          outputs: [buildOutput],
+        }),
+      ],
+    });
+  }
+}
