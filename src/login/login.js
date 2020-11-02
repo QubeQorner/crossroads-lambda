ca30e19fd1f9ddcdd1063216f60bcfab92e09c70
Gareth Walkersmith <gpsmith83@gmail.com>
2020-11-02 10:22:01 +0000

    Extract secret from returned secrets manager object

--- src/login/login.js
+++ src/login/login.js
@@ -10,6 +10,18 @@ let response;
 
 /**
  *
+ * @param record - record from AWS RDSDataServices
+ * @returns {undefined}
+ */
+function buildUserData(record) {
+    return {
+        "player_uuid": record[1].stringValue,
+        "username": record[2].stringValue
+    };
+}
+
+/**
+ *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
@@ -30,7 +42,7 @@ exports.handler = async (event, context) => {
         const secretParam = {
             SecretId: 'crossroads-prod'
         };
-        const jwtSecret = await secretsManager.getSecretValue(secretParam).promise().catch((err) => {
+        const secretStr = await secretsManager.getSecretValue(secretParam).promise().catch((err) => {
             return { // Error response
                 statusCode: 401,
                 headers: {
@@ -41,6 +53,8 @@ exports.handler = async (event, context) => {
                 }),
             };
         });
+
+        const jwtSecret = JSON.parse(data.SecretString).jwtSecret;
 
         const rdsParams = {
             secretArn: 'arn:aws:secretsmanager:eu-central-1:597880501761:secret:rds-db-credentials/cluster-R2Q7TW2NSOTZSLDRPBDOIMSQE4/crossroads_dba-sJjF7R',
@@ -54,8 +68,6 @@ exports.handler = async (event, context) => {
               }
             }]
         }
-
-        // const hash = await bcrypt.hash(password, 10);
 
         const rdsDataService = new AWS.RDSDataService();
         const userData = await rdsDataService.executeStatement(rdsParams).promise().catch((err) => {
@@ -83,22 +95,23 @@ exports.handler = async (event, context) => {
             return response;
         }
 
-        const user = userData.records[0];
+        const user = buildUserData(userData.records[0]);
         // Authenticate user
         // const user = users.login(username, password);
 
-        // Issue JWT
-        const token = jwt.sign({ user }, jwtSecret, { expiresIn: JWT_EXPIRATION_TIME });
-        response = { // Success response
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-            body: JSON.stringify({
-                token,
-            }),
-        };
-
+        if (bcrypt.compareSync(password, userData.records[0][3].stringValue)){
+            // Issue JWT
+            const token = jwt.sign({ user }, jwtSecret, { expiresIn: JWT_EXPIRATION_TIME });
+            response = { // Success response
+                statusCode: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                },
+                body: JSON.stringify({
+                    token,
+                }),
+            };
+        }
     } catch (e) {
         response = { // Error response
             statusCode: 401,
